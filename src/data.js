285ff178668ad460ee3b589d4f5a2bbe78d76cc7
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-04-02 15:21:41 +0300

    fix muxic svg

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const data = {
     title: "Order Summary",
     para: "You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!",
-    img:"./src/assets/images/icon-music.svg",
+    img:"src/assets/images/icon-music.svg",
     plan: "Annual Plan",
     price: '$59.99/year',
     change: 'Change',
